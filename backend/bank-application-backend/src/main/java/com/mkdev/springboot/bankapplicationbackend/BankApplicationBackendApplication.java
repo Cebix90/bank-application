1d490ba9673f38e9273e051904fedba349f23a9a
Cebix90 <cebulmat@gmail.com>
2024-11-17 13:25:06 +0100

    Initial commit - backend and frontend structure

---
+++ backend/bank-application-backend/src/main/java/com/mkdev/springboot/bankapplicationbackend/BankApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mkdev.springboot.bankapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ backend/bank-application-backend/src/test/java/com/mkdev/springboot/bankapplicationbackend/BankApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkdev.springboot.bankapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
