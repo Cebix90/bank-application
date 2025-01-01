f731dc6d0b4a3d58e086d3e0e9d7fff1e7bad35d
KamilDoniek <117510031+KamilDoniek@users.noreply.github.com>
2025-01-01 21:13:29 +0100

    Frontend/feature/edit login register pages (#14)
    * [Added] logout
    
    * [Edit] Context

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,23 +1,44 @@
-import  { createContext, useState, useContext } from "react";
+// <<<<<<< Frontend/feature/EditLoginRegisterPages
+// // import  { createContext, useState, useContext } from "react";
+// =======
+// import  { createContext, useState, useContext } from "react";
 
-// Tworzymy kontekst
-const UserContext = createContext();
+// // Tworzymy kontekst
+// const UserContext = createContext();
+// >>>>>>> main
 
-// Custom hook do łatwego dostępu do kontekstu
-export const useUser = () => useContext(UserContext);
+// // Custom hook do łatwego dostępu do kontekstu
+// export const useUser = () => useContext(UserContext);
 
-export const UserProvider = ({ children }) => {
-  const [userName, setUserName] = useState(null);
+// export const UserProvider = ({ children }) => {
+//   const [userName, setUserName] = useState(null);
 
-  // Funkcja logowania - ustawia nazwę użytkownika
-  const login = (name) => setUserName(name);
+// <<<<<<< Frontend/feature/EditLoginRegisterPages
+// // export const UserProvider = ({ children }) => {
+// //   const [userName, setUserName] = useState(null);
+// =======
+//   // Funkcja logowania - ustawia nazwę użytkownika
+//   const login = (name) => setUserName(name);
+// >>>>>>> main
 
-  // Funkcja wylogowania - resetuje stan użytkownika
-  const logout = () => setUserName(null);
+//   // Funkcja wylogowania - resetuje stan użytkownika
+//   const logout = () => setUserName(null);
 
-  return (
-    <UserContext.Provider value={{ userName, login, logout }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+// <<<<<<< Frontend/feature/EditLoginRegisterPages
+// //   // Funkcja wylogowania - resetuje stan użytkownika
+// //   const logout = () => setUserName(null);
+
+// //   return (
+// //     <UserContext.Provider value={{ userName, login, logout }}>
+// //       {children}
+// //     </UserContext.Provider>
+// //   );
+// // };
+// =======
+//   return (
+//     <UserContext.Provider value={{ userName, login, logout }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+// >>>>>>> main
