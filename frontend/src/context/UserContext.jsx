bace4a479ecc04f10e41d7f6cc6f982d2fa26663
KamilDoniek <117510031+KamilDoniek@users.noreply.github.com>
2025-01-01 21:07:47 +0100

    [Added] logout (#13)

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,25 +1,23 @@
-// // src/context/UserContext.js
+import  { createContext, useState, useContext } from "react";
 
-// import { createContext, useState, useContext } from "react";
+// Tworzymy kontekst
+const UserContext = createContext();
 
-// // Tworzymy kontekst
-// const UserContext = createContext();
+// Custom hook do łatwego dostępu do kontekstu
+export const useUser = () => useContext(UserContext);
 
-// // Custom hook do łatwego dostępu do kontekstu
-// export const useUser = () => useContext(UserContext);
+export const UserProvider = ({ children }) => {
+  const [userName, setUserName] = useState(null);
 
-// export const UserProvider = ({ this.props }) => {
-//   const [userName, setUserName] = useState(null);
+  // Funkcja logowania - ustawia nazwę użytkownika
+  const login = (name) => setUserName(name);
 
-//   // Funkcja logowania - ustawia nazwę użytkownika
-//   const login = (name) => setUserName(name);
+  // Funkcja wylogowania - resetuje stan użytkownika
+  const logout = () => setUserName(null);
 
-//   // Funkcja wylogowania - resetuje stan użytkownika
-//   const logout = () => setUserName(null);
-
-//   return (
-//     <UserContext.Provider value={{ userName, login, logout }}>
-//       {props}
-//     </UserContext.Provider>
-//   );
-// };
+  return (
+    <UserContext.Provider value={{ userName, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
